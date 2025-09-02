ce9a7229d437832d7efa067e7ce095cebeebdfc3
SM Siddalingaswamy <smsiddu2266@gmail.com>
2025-09-02 18:40:21 +0000

    While signup, when click on create account showing failed. Its not creat

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -28,13 +28,14 @@ async function addUserToDatabase(user: UserRecord, extraData: { firstName: strin
     const client = await clientPromise;
     const db = client.db();
     const usersCollection = db.collection("users");
+    const displayName = `${extraData.firstName} ${extraData.lastName}`;
 
     const newUser = {
       uid: user.uid,
       email: user.email,
       firstName: extraData.firstName,
       lastName: extraData.lastName,
-      displayName: user.displayName,
+      displayName: displayName,
       college: extraData.college,
       place: extraData.place,
       createdAt: new Date(),
