f239c4a3c70d614dac5a98018e5303a3c9fcae4f
SM Siddalingaswamy <smsiddu2266@gmail.com>
2025-09-02 18:36:24 +0000

    Make this text in single line

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-card px-4 md:px-6 z-50">
-      <nav className="flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 w-full">
+      <nav className="gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 w-full">
         <Link
           href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
